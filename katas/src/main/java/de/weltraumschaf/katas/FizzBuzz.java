bd5025d7ae9e850b28361ce2dc6d22e1c2eb364c
Sven Strittmatter <Sven.Strittmatter@icongmbh.de>
2014-05-06 18:29:23 +0200

    Add posible solution.

--- katas/src/main/java/de/weltraumschaf/katas/FizzBuzz.java
+++ katas/src/main/java/de/weltraumschaf/katas/FizzBuzz.java
@@ -1,11 +1,20 @@
 package de.weltraumschaf.katas;
 
 /**
- * You can find a possible solution here: http://technologyconversations.com/2014/03/12/java-tutorial-through-katas-fizz-buzz-easy/
+ * You can find a possible solution here:
+ * http://technologyconversations.com/2014/03/12/java-tutorial-through-katas-fizz-buzz-easy/
  */
 final class FizzBuzz {
 
     public static String getResult(int number) {
-        return null;
+        if (number % 15 == 0) {
+            return "fizzbuzz";
+        } else if (number % 3 == 0) {
+            return "fizz";
+        } else if (number % 5 == 0) {
+            return "buzz";
+        }
+
+        return Integer.toString(number);
     }
 }
